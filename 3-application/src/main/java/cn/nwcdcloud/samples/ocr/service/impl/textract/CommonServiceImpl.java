e3695558ecd102503c8bb3cb9a22b6ce371a52ef
dikers <dikers@gmail.com>
2020-12-16 11:32:28 +0800

    添加解析功能

--- 3-application/src/main/java/cn/nwcdcloud/samples/ocr/service/impl/textract/CommonServiceImpl.java
+++ 3-application/src/main/java/cn/nwcdcloud/samples/ocr/service/impl/textract/CommonServiceImpl.java
@@ -1,11 +1,16 @@
 package cn.nwcdcloud.samples.ocr.service.impl.textract;
 
+import cn.nwcdcloud.commons.utils.BlockItemUtil;
+import cn.nwcdcloud.commons.utils.ParseJsonUtil;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONArray;
 
 import cn.nwcdcloud.commons.lang.Result;
 import cn.nwcdcloud.samples.ocr.service.impl.CommonService;
+
+import java.util.List;
 
 @Service
 public class CommonServiceImpl implements CommonService {
@@ -15,9 +20,18 @@ public class CommonServiceImpl implements CommonService {
 	@Override
 	public Result parse(String type, JSONArray jsonArray) {
 		Result result = new Result();
-		
-		String data = "[[{\"name\":\"姓名\",\"value\":\"李四\"}]]";// TODO 需要返回多张图片结果
-		result.setData(data);
+
+		JSONObject jsonObject = (JSONObject) jsonArray.getJSONObject(0);
+		List<JSONObject> blockItemList = BlockItemUtil.getBlockItemList(jsonObject, 1124, 800);
+		ParseJsonUtil parseJsonUtil = new ParseJsonUtil(1124, 800, blockItemList);
+
+		JSONArray resultArray = new JSONArray();
+		resultArray.add(parseJsonUtil.extractValue(blockItemList));
+
+//		String data = "[[{\"name\":\"姓名\",\"value\":\"李四\"}]]";
+		// TODO 需要返回多张图片结果
+		System.out.println("****************** "+ resultArray.toJSONString());
+		result.setData(resultArray.toJSONString());
 		return result;
 	}
 
