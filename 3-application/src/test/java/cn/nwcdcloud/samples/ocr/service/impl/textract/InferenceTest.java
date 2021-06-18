bf812ec4a5d472638fbc5f213178e8e75bad429b
nowfox <nowfox@163.com>
2021-06-18 10:39:42 +0800

    生成门诊JSON

---
+++ 3-application/src/test/java/cn/nwcdcloud/samples/ocr/service/impl/textract/InferenceTest.java
@@ -0,0 +1,65 @@
+package cn.nwcdcloud.samples.ocr.service.impl.textract;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cn.nwcdcloud.commons.http.HttpClientUtils;
+
+public class InferenceTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(InferenceTest.class);
+
+	public static void main(String[] args) throws ParseException, IOException {
+		if (args.length != 2) {
+			logger.error("请输入输入路径");
+			return;
+		}
+		String dirInput = args[0];
+		String dirOutput = args[1];
+		File folderInput = new File(dirInput);
+		File folderOutput = new File(dirOutput);
+		if (!folderOutput.exists()) {
+			folderOutput.mkdirs();
+		}
+		CloseableHttpClient httpClient = (CloseableHttpClient) HttpClientUtils.getHttpClient();
+		String url = "http://127.0.0.1/inference/predict";
+
+		for (String fileName : folderInput.list()) {
+			if (!isImage(fileName)) {
+				continue;
+			}
+			HttpPost httpPost = new HttpPost(url);
+			httpPost.setHeader("Content-Type", "image/jpeg");
+			httpPost.setEntity(new FileEntity(new File(dirInput + File.separator + fileName)));
+			CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+			String result = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+			String filePrefix = fileName.split("\\.")[0];
+			BufferedWriter out = new BufferedWriter(new FileWriter(dirOutput + File.separator + filePrefix + ".json"));
+			out.write(result);
+			out.close();
+		}
+	}
+
+	private static boolean isImage(String fileName) {
+		String[] extNames = { "jpg", "jpeg", "bmp", "png" };
+		fileName = fileName.toLowerCase();
+		for (String extName : extNames) {
+			if (fileName.endsWith(extName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ 3-application/src/test/java/cn/nwcdcloud/samples/ocr/service/impl/textract/Rename.java
@@ -0,0 +1,43 @@
+package cn.nwcdcloud.samples.ocr.service.impl.textract;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Rename {
+	private static final Logger logger = LoggerFactory.getLogger(Rename.class);
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			logger.error("请输入需要重命名路径");
+			return;
+		}
+		String dir = args[0];
+		File folder = new File(dir);
+		int i = 101;
+		for (String fileName : folder.list()) {
+			String extName = getImageExtName(fileName);
+			if (extName == null) {
+				continue;
+			}
+			File oldFile = new File(dir + File.separator + fileName);
+			String newFileName = String.format("%03d", i);
+			File newFile = new File(dir + File.separator + newFileName + "." + extName);
+			oldFile.renameTo(newFile);
+			i++;
+		}
+	}
+
+	private static String getImageExtName(String fileName) {
+		String[] extNames = { "jpg", "jpeg", "bmp", "png" };
+		fileName = fileName.toLowerCase();
+		for (String extName : extNames) {
+			if (fileName.endsWith(extName)) {
+				return extName;
+			}
+		}
+		return null;
+	}
+
+}
